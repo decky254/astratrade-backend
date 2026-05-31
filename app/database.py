b9a994ad228d369a645ef4243c05cff29249f9e2
decky254 <derricknjenga17@gmail.com>
2026-06-01 01:59:36 +0300

    Update database.py

--- app/database.py
+++ app/database.py
@@ -3,7 +3,6 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-# Get the URL from Render's Environment Variable
 SQLALCHEMY_DATABASE_URL = os.getenv("DATABASE_URL")
 
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
